6eb9f880bc4027a0e18439fb679d7c0b599e5ddb
djidji09 <djelouahyoucef1@gmail.com>
2023-12-02 18:39:54 +0100

    intializing the project still some problemes though

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 # Configure application
 app = Flask(__name__)
@@ -10,31 +9,28 @@ db = SQLAlchemy(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    """Show portfolio of stocks"""
     if request.method == "GET":
         return render_template("index.html")
     else:
-        redirect("login")
+        return redirect(url_for("login"))
 
 
-@app.route("/sign_up", methods=["GET", "POST"])
+@app.route("sign_up", methods=["GET", "POST"])
 def sign_up():
-    """Show portfolio of stocks"""
     if request.method == "GET":
         return render_template("sign_up.html")
     else:
-        redirect("/login")
+        return redirect("/login")  # Added return statement
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("login", methods=["GET", "POST"])
 def login():
-    """Show portfolio of stocks"""
     if request.method == "GET":
         return render_template("login.html")
     else:
-        redirect("/")
+        return redirect("/")  # Added return statement
 
 
-# running the debuger
+# running the debugger
 if __name__ == '__main__':
     app.run(debug=True)
